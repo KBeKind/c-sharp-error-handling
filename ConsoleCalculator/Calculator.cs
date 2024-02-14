3243bf2e6bfaa3587b9a09fdca006e3d57b54db1
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-02-14 11:51:19 -0600

    commit

---
+++ ConsoleCalculator/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleCalculator;
+
+public class Calculator
+{
+    public int Calculate(int number1, int number2, string operation)
+    {
+        if (operation == "/")
+        {
+            return Divide(number1, number2);
+        }
+        else
+        {
+
+            var ex = new ArgumentOutOfRangeException(nameof(operation), "The mathematical operator is not supported.");
+
+            throw ex;
+            //Console.WriteLine("Unknown operation.");
+            //return 0;
+        }
+    }
+
+    private int Divide(int number, int divisor) => number / divisor;
+}
+
